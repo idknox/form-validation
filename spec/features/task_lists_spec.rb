1d8ffea31f26f3e9f520bc8c3885e51d361f00fd
Ian Knox <knoxid@gmail.com>
2014-08-04 14:52:23 -0600

    About page added

--- spec/features/task_lists_spec.rb
+++ spec/features/task_lists_spec.rb
@@ -18,3 +18,14 @@ feature 'Task lists' do
   end
 
 end
+
+feature "About Page" do
+  scenario "User can view About Page" do
+    visit "/"
+    click_link "About"
+    expect(page).to have_content "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tristique lectus, vitae tristique urna. Integer
+  facilisis bibendum urna, vitae tempor sapien blandit sed. Nulla eros magna, suscipit eget magna sed, placerat dictum
+  elit. Suspendisse potenti. Maecenas consequat magna eu urna rhoncus tristique. Proin hendrerit in quam quis imperdiet.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et hendrerit est."
+  end
+end
