c361ea3fac5ba7145d339a3cb421f8a13dc864ea
Ian Knox <knoxid@gmail.com>
2014-08-07 01:21:24 -0600

    refactored to about controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "task_lists#index"
   get "/signin" => "sessions#new", as: :signin
   get "/signout" => "sessions#destroy", as: :signout
-  get "/about" => "sessions#about"
+  get "/about" => "about#show"
   post "/signin" => "sessions#create"
   patch "/task_lists/:task_list_id/tasks/:id/completed" => "tasks#complete", as: :task_complete
   get "/task_lists/:id/completed" => "task_lists#show_completed", as: :task_list_completed
